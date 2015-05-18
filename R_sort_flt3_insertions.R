818ffd861eaaa20f192d3568c92762617960c600
Irina Krier <irina.krier@gmail.com>
2015-05-18 10:26:04 +0200

    made the export file

--- R_sort_flt3_insertions.R
+++ R_sort_flt3_insertions.R
@@ -20,7 +20,7 @@
 
 rm(list=ls())
 
-data=read.table("parsed_flt3_SIs_all_libs.csv",sep="\t",header=1,fill = T)
+data=read.table("parsed_flt3_SIs_all_libs.csv",sep="\t",header=1,fill = T,stringsAsFactors = F)
 
 heatmap(as.matrix(data[,grep("upall",colnames(data))]+data[,grep("downall",colnames(data))]),Rowv = NA, Colv = NA)
 
@@ -33,7 +33,7 @@ pos=list()
 range=seq(1,5001,200)
 for(i in 1:length(range))
 {
-  pos[[i]]=which(colSums(as.matrix(data[,grep("upall",colnames(data))]+data[,grep("downall",colnames(data))]))>range[i])
+  pos[[i]]=which(colSums(as.matrix(data[,grep("upall",colnames(data))]+data[,grep("downall",colnames(data))]))>=range[i])
 }
 
 plot(range,lapply(pos,length),type="l",xlab="Threshold applied",ylab="Number of samples with insertions")
@@ -46,9 +46,6 @@ npv=lapply(pos,function(x){sum(!names(x)%in%clinically_pos)/length(x)})
 FPtype1=lapply(pos,function(x){sum(clinically_neg%in%names(x))/length(clinically_neg)})
 FNtype2=lapply(pos,function(x){sum(!clinically_pos%in%names(x))/length(clinically_pos)})
 
-plot(recall,precision,type="l")
-plot(FPtype1,recall,type="l")
-
 pdf("PrecisionRecallSPec.pdf")
 plot(range,recall,type="l",ylim=c(0,1),xlab="Cutoff sum",ylab="")
 points(range,precision,type="l",col=2)
@@ -60,12 +57,38 @@ abline(v=1201)
 legend("bottomleft",legend=c("Recall","Precision","Specificity"),col=c(1,2,3),lty = 1)
 dev.off()
 
-clinically_pos[!clinically_pos%in%names(pos[[2]])]
+clinically_pos[!clinically_pos%in%names(pos[[7]])]
 #We never can find 28, 31 and 80 with this filtering.
 #Indeed in the filtered file they don't really show up.
 
-names(pos[[7]])
+positives=as.character(sapply(names(pos[[7]]),function(x){strsplit(x,"_")[[1]][3]}))
 recall[[7]]
 precision[[7]]
 specificity[[7]]
 
+dataout=as.data.frame(data[,c("chromosome","startrange","endrange","sequence","length")])
+
+selectall=grep("all",colnames(data))
+selectuniq=grep("uniq",colnames(data))
+libnames=sapply(colnames(data)[grep("upall",colnames(data))],function(x){{strsplit(x,"_")[[1]][3]}})
+
+for(lib in 1:length(libnames))
+{
+  dataout[,paste("Lib_",libnames[lib],"_all",sep="")]=rowSums(data[,selectall[lib*2+c(-1,0)]])
+  dataout[,paste("Lib_",libnames[lib],"_unique",sep="")]=rowSums(data[,selectuniq[lib*2+c(-1,0)]])
+  dataout[,paste("Lib_",libnames[lib],"_MutationState",sep="")]=as.numeric(rowSums(data[,selectall[lib*2+c(-1,0)]]))>=201
+}
+
+dataoutsmall=dataout[rowSums(dataout[grep("MutationState",colnames(dataout))])>0,]
+dataoutsmall=rbind(dataoutsmall,c("TOTAL",rep("",4),colSums(dataout[,-(1:5)])))
+
+for(lib in 1:length(libnames))
+{
+  dataoutsmall[nrow(dataoutsmall),paste("Lib_",libnames[lib],"_MutationState",sep="")]=c("Normal","Mutated")[1+(as.numeric(dataoutsmall[nrow(dataoutsmall),paste("Lib_",libnames[lib],"_all",sep="")])>=1201)]
+  dataoutsmall[-nrow(dataoutsmall),paste("Lib_",libnames[lib],"_MutationState",sep="")]=c("Absent","Present")[1+as.numeric(as.logical(dataoutsmall[-nrow(dataoutsmall),paste("Lib_",libnames[lib],"_MutationState",sep="")]))]
+}
+
+write.table(dataoutsmall,file="Results_FLT3_summary.csv",sep="\t",quote=F,row.names=F)
+ 
+#Something weird with lib84 where 447 supporting reads but 0 unique!
+#42 is actually negative.
